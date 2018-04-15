392af9a74616e7a745f223c08ee80196244f9db4
donrestarone <shashikejayatunge@gmail.com>
2018-04-15 13:45:18 -0400

    individual definition pages work

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,30 +1,55 @@
 class EntriesController < ApplicationController
 	
 	def index 
+		@entries = Entry.all
 		render :index
 	end 
 
 	def show 
+		@entry = Entry.find(params[:id])
 		render :show
 	end 
 
 	def new 
+		@entry = Entry.new 
 		render :new 
 	end 
 
 	def create 
-		redirect_to entries_url
+		@entry = Entry.new 
+		@entry.word = params[:entry][:word]
+		@entry.definition = params[:entry][:definition]
+		@entry.language = params[:entry][:langauge]
+ 
+		if @entry.save
+			redirect_to entries_url
+		else 
+			redirect_to entries_url
+		end 
 	end 
 
 	def edit 
+		@entry = Entry.find(params[:id])
 		render :edit
 	end 
 
 	def update 
-		redirect_to entry_url(params[:id])
+		@entry = Entry.find(params[:id])
+
+		@entry.word = params[:entry][:word]
+		@entry.definition = params[:entry][:definition]
+		@entry.language = params[:entry][:language]
+
+		if @entry.save
+			redirect_to entry_url(params[:id])
+		else 
+			redirect_to entry_url(params[:id])
+		end 
 	end	 
 
 	def destroy
+		@entry = Entry.find(params[:id])
+		@location.destroy
 		redirect_to entries_url
 	end 
 end
